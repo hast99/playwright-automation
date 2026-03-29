8aa0213ebec70c9dffea26a5056d9128690aef39
HAST <hastbro99@gmail.com>
2026-03-29 22:07:25 +0700

    add test data

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,18 +1,19 @@
 import {expect, test} from "@playwright/test";
 import {LoginPage} from "../pages/login.page";
+import {users} from "../utils/test-data";
 
 test.describe("Login", () => {
     test('[PC] Login Success', async ({page}) => {
         const loginPage = new LoginPage(page);
         await loginPage.goto();
-        await loginPage.login('tomsmith', 'SuperSecretPassword!');
+        await loginPage.login(users.valid.username, users.valid.password);
         await expect(page).toHaveURL(/secure/); //untuk memastikan setelah login sampai di url /secure
     });
 
     test('[NC] Login Failed', async ({page}) => {
         const loginPage = new LoginPage(page);
         await loginPage.goto();
-        await loginPage.login('test', 'test!');
+        await loginPage.login(users.invalid.username, users.invalid.password);
         await expect(await loginPage.getMessageSuccess()).toContainText('Your username is invalid!');
     });
 });
---
+++ utils/test-data.ts
@@ -0,0 +1,10 @@
+export const users = {
+    valid: {
+        username: 'tomsmith',
+        password: 'SuperSecretPassword!'
+    },
+    invalid: {
+        username: 'wrong',
+        password: 'wrong'
+    }
+};
